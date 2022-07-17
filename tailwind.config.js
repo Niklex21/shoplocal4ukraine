a2e51f55219b5c619d2da7d837ef443214a617f6
Alex Nikanov <alekseynikanov.21@gmail.com>
2022-07-17 17:13:07 -0400

    style: :lipstick: Added custom theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: {
+          DEFAULT: "#232327",
+          inverted: "#FFFDF5"
+        },
+        background: {
+          DEFAULT: "#FFEBAF",
+          inverted: "#232335"
+        },
+        accent: "#D7E6F5"
+      },
+    },
   },
   plugins: [],
 }
