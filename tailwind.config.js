02cd1ad07c2f80c6b334972ca3249b773ed52a0f
Restyled.io <commits@restyled.io>
2022-10-17 19:43:32 +0000

    Restyled by clang-format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
+  content : [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      height: {
-        'card': '30rem',
+  theme : {
+    extend : {
+      height : {
+        'card' : '30rem',
       },
-      colors: {
-        text: {
-          DEFAULT: "#232327",
-          inverted: "#FFFDF5"
-        },
-        background: {
-          DEFAULT: "#FFEBAF",
-          inverted: "#232335"
-        },
-        accent: "#D7E6F5",
-        'ukraine-yellow': "#FFD101",
-        "ukraine-blue": "#005EB8"
+      colors : {
+        text : {DEFAULT : "#232327", inverted : "#FFFDF5"},
+        background : {DEFAULT : "#FFEBAF", inverted : "#232335"},
+        accent : "#D7E6F5",
+        'ukraine-yellow' : "#FFD101",
+        "ukraine-blue" : "#005EB8"
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/typography')
+  plugins : [
+    require('@tailwindcss/line-clamp'), require('@tailwindcss/typography')
   ],
 }
