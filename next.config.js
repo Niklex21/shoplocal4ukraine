8607abf361bbb0f6c28b67aa5bc7f0d71554bd57
Restyled.io <commits@restyled.io>
2022-10-22 03:17:28 +0000

    Restyled by prettier

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 // has to be const/require because import is not recognized in next.config.js
-const {withAxiom} = require('next-axiom');
+const { withAxiom } = require("next-axiom");
 
 const nextConfig = {
-  reactStrictMode : true,
-  images : {domains : [ 'dl.airtable.com' ]}
-}
+  reactStrictMode: true,
+  images: { domains: ["dl.airtable.com"] },
+};
 
-                   module.exports = withAxiom(nextConfig)
+module.exports = withAxiom(nextConfig);
