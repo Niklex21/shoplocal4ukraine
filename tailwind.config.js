039ff72632a8ea48b254902086ba8bb5e41826c6
Alex Nikanov <alekseynikanov.21@gmail.com>
2022-11-01 19:55:10 -0400

    build: :construction: (wip) Trying to add custom e-ukraine font, not working yet for some obscure reason

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'e-ukraine': ['e-Ukraine', 'sans-serif']
+      },
       height: {
         card: "30rem",
       },
