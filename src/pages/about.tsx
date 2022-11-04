5b03c90ef558dec62f5ee2b5bb0f46b49e094f60
Alex Nikanov <alekseynikanov.21@gmail.com>
2022-11-04 19:11:19 -0400

    docs: :speech_balloon: Updated Artem's description text

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -14,7 +14,7 @@ const team : Array<Person> = [
         name: "Artem Dinh",
         profilePictureURL: ImageArtem.src,
         role: "Co-founder/Ops Director",
-        description: "When he gets some time off, Artem loves hanging out with his friends and exploring the city on his bike.",
+        description: "Artem plays ping pong, hangs out with his friends, engages in geopolitical discussions, and spams thousands of Russians with anti-propaganda.",
         gifURL: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f67a9a51-af2d-4337-bc66-d9f7a602cb72/peepo-ukraine.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221104%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221104T044320Z&X-Amz-Expires=86400&X-Amz-Signature=df4ce1bfbbc2ac0c0b14aaf356ca520f429522089dfc08d6a4b26ad38e56a299&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22peepo-ukraine.gif%22&x-id=GetObject",
         email: "tdinh02@tufts.edu",
         linkedin: "https://www.linkedin.com/in/long-artem-dinh/"
