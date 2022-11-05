156272785d2f516612f397867210d3856e093e81
Restyled.io <commits@restyled.io>
2022-11-05 05:39:22 +0000

    Restyled by clang-format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
+  content : [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'e-ukraine': ['e-Ukraine', 'sans-serif']
+  theme : {
+    extend : {
+      fontFamily : {'e-ukraine' : [ 'e-Ukraine', 'sans-serif' ]},
+      height : {
+        card : "30rem",
       },
-      height: {
-        card: "30rem",
+      colors : {
+        text : {DEFAULT : "#232327", inverted : "#FFFDF5"},
+        background : {DEFAULT : "#FFEBAF", inverted : "#232335"},
+        accent : "#D7E6F5",
+        "ukraine-yellow" : "#FFD700",
+        "ukraine-blue" : "#0057B8",
       },
-      colors: {
-        text: { DEFAULT: "#232327", inverted: "#FFFDF5" },
-        background: { DEFAULT: "#FFEBAF", inverted: "#232335" },
-        accent: "#D7E6F5",
-        "ukraine-yellow": "#FFD700",
-        "ukraine-blue": "#0057B8",
+      dropShadow : ({theme}) =>
+          ({'button' : `4px 4px 0px ${theme('colors')['ukraine-blue']}`}),
+      transitionProperty : {'filter' : 'filter'},
+      backgroundImage : {
+        'gradient-radial' : 'radial-gradient(var(--tw-gradient-stops))',
       },
-      dropShadow: ({ theme }) => ({
-        'button': `4px 4px 0px ${ theme('colors')['ukraine-blue'] }`
-      }),
-      transitionProperty: {
-        'filter': 'filter'
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      },
-      backgroundPosition: {
-        'bottom-edge': "0% 0% 0% 150%"
-      },
-      backgroundSize: {
-        "size-2x": "100% 300%"
-      }
+      backgroundPosition : {'bottom-edge' : "0% 0% 0% 150%"},
+      backgroundSize : {"size-2x" : "100% 300%"}
     },
   },
-  plugins: [
+  plugins : [
     require("@tailwindcss/line-clamp"),
     require("@tailwindcss/typography"),
   ],
