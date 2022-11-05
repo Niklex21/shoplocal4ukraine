24d2a98c4ab12bb1d40da306f36af3a561acf751
Alex Nikanov <alekseynikanov.21@gmail.com>
2022-11-04 20:08:33 -0400

    refactor: :recycle: Removed the business container because it's only used in a single place

--- src/components/business/BusinessContainer.tsx
+++
@@ -1,9 +0,0 @@
-import { twMerge } from "tailwind-merge";
-
-export default function BusinessContainer({ className, children }: any) {
-  return (
-    <div className={ twMerge('flex flex-row flex-wrap gap-6 my-20 md:mx-auto px-1 md:px-12 justify-center md:justify-start', className) }>
-      {children}
-    </div>
-  )
-}
