18485289ec3c579691351f826854031d08c5de42
Restyled.io <commits@restyled.io>
2022-11-11 07:51:02 +0000

    Restyled by clang-format

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 // has to be const/require because import is not recognized in next.config.js
-const { withAxiom } = require("next-axiom");
+const {withAxiom} = require("next-axiom");
 
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [
-      "dl.airtable.com",
-      "thumbs.gfycat.com",
-      "s3.us-west-2.amazonaws.com",
+  reactStrictMode : true,
+  images : {
+    domains : [
+      "dl.airtable.com", "thumbs.gfycat.com", "s3.us-west-2.amazonaws.com",
       "v5.airtableusercontent.com"
     ],
   },
