760b410d5f80c0c880377639f26f3c8581700b45
Alex Nikanov <alekseynikanov.21@gmail.com>
2022-11-10 22:42:02 -0500

    chore: :memo: Updated docstrings & changed the name of the atoms/businesses init file

--- src/atoms/businesses.ts
+++ src/atoms/businesses.ts
@@ -5,25 +5,35 @@ import Fuse from "fuse.js"
 import { atomWithHash } from "jotai/utils";
 import { findBusinessById, serializeBusinessModel, serializedToSearchSerialized } from "@utils/utils";
 
-// TODO: fix comments into docstrings
-
-// stores whether or not the map is being moved right now
+/** 
+ * Stores whether or not the map is being moved right now.
+ */
 const atomMapDragState = atom<MapDragState>(MapDragState.Off)
 
-// stores the currently selected business ID
-// delayInit has to be true to avoid problems with hydration (https://github.com/pmndrs/jotai/issues/739#issuecomment-929260696)
+/**
+ * Stores the currently selected business ID.
+ * Note: delayInit has to be true to avoid problems with hydration (https://github.com/pmndrs/jotai/issues/739#issuecomment-929260696) 
+ */
 const atomSelectedBusinessID = atomWithHash<string>("business_id", "", { delayInit: true })
 
-// stores the current search query in the search bar in the main businesses view
+/**
+ * Stores the current search query in the search bar in the main businesses view.
+ */
 const atomSearchQuery = atomWithHash<string>("search", "", { delayInit: true })
 
-// stores the selected categories that filter the businesses
+/**
+ * Stores the selected categories that filter the businesses.
+ */
 const atomSelectedCategories = atomWithHash<Array<BusinessCategory>>("categories", [], { delayInit: true })
 
-// stores all the businesses currently available, should only be modified once
+/**
+ * Stores all the businesses currently available, should only be modified once.
+ */
 const atomAllBusinesses = atom<Array<BusinessModel>>([])
 
-// a read-only item that stores the currently selected business
+/**
+ * A read-only item that stores the currently selected business.
+ */
 const atomCurrentBusiness = atom<SerializedBusinessModel>(
     (get) => serializeBusinessModel(
         findBusinessById(
@@ -33,7 +43,9 @@ const atomCurrentBusiness = atom<SerializedBusinessModel>(
     )
 )
 
-// a read-only atom that stores businesses that are filtered according to the currently selected categories
+/**
+ * A read-only atom that stores businesses that are filtered according to the currently selected categories
+ */
 const atomFilteredBusinesses = atom<Array<BusinessModel>>(
     (get) => {
         return get(atomSelectedCategories).length === 0
@@ -42,7 +54,9 @@ const atomFilteredBusinesses = atom<Array<BusinessModel>>(
     }
 )
 
-// a read-only atom that stores the FuseSearch object derived from current filtered businesses
+/**
+ * A read-only atom that stores the FuseSearch object derived from current filtered businesses.
+ */
 const atomFuseSearch = atom<Fuse<SerializedBusinessModel>>(
     (get) => new Fuse<SerializedBusinessModel>(
         get(atomFilteredBusinesses).map(
@@ -63,7 +77,9 @@ const atomFuseSearch = atom<Fuse<SerializedBusinessModel>>(
     )
 )
 
-// a read-only atom that stores the businesses with the search query applied on
+/**
+ * A read-only atom that stores the businesses with the search query applied on.
+ */
 const atomSearchedBusinesses = atom<Array<SearchedSerializedBusiness>>(
     (get) => {
         return get(atomSearchQuery) === ""
--- src/atoms/global.ts
+++ src/atoms/global.ts
@@ -1,6 +1,9 @@
 import { PanelState } from "@appTypes/businesses";
 import { atom } from "jotai";
 
+/**
+ * Stores the state of the menu panel.
+ */
 const menuStateAtom = atom<PanelState>(PanelState.Closed)
 
 export { menuStateAtom }
