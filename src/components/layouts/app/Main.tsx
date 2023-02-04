c2a9a32765acc625e9b3e6d1fd38dab896949d31
Alex Nikanov <alekseynikanov.21@gmail.com>
2023-02-03 22:06:23 -0500

    feat: :chart_with_upwards_trend: Added mailchimp analytics

--- src/components/layouts/app/Main.tsx
+++ src/components/layouts/app/Main.tsx
@@ -25,6 +25,13 @@ export default function AppMainLayout({ children }: Props) {
                 <link rel="shortcut icon" href={ favicon.src } />
                 <title key="title">{ strings.all.title }</title>
             </Head>
+            <Script id="mcjs">
+                {`
+                    !function(c,h,i,m,p){
+                        m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)
+                    }(document,"script","https://chimpstatic.com/mcjs-connected/js/users/5294169e3c5e26832ef9cc671/6ac9341a1aa166e2f49201439.js");
+                `}
+            </Script>
             <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-P68Q0F71ED" />
             <Script id="google-analytics" strategy="afterInteractive">
                 {`
